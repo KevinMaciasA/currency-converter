c5e902339df4ae7795af7bfa400feb2ac2d41ef2
Kevin <kevin.macias.a@gmail.com>
2024-04-13 20:53:32 -0700

    Add Env.java util to read info from a .env file

---
+++ src/env/Env.java
@@ -0,0 +1,21 @@
+package env;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+abstract public class Env {
+  public static String read() {
+    Properties props = new Properties();
+    try {
+      FileInputStream input = new FileInputStream(".env");
+      System.out.println(input);
+      props.load(input);
+      input.close();
+      return props.getProperty("EXCHANGE_API_KEY");
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
